3412867618face74050402656533f6fbbc1447de
ynunokawa <yusuke_nunokawa@esrij.com>
2015-08-20 16:17:26 +0900

    test

--- js/map.js
+++ js/map.js
@@ -1,11 +1,25 @@
 $( document ).ready(function() {
 
-  var map = L.map('map', {
-    zoomControl: false
-  }).setView([35, 139], 5);
+  navigator.geolocation.getCurrentPosition(zoomToLocation, locationError);
 
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+  function zoomToLocation(location) {
+    var map = L.map('map', {
+      zoomControl: false
+    }).setView([location.coords.latitude, location.coords.longitude], 16);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
+
+  function locationError(error) {
+    var map = L.map('map', {
+      zoomControl: false
+    }).setView([35, 139], 5);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
 
 });
